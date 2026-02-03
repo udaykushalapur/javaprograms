c6aeba91972988359a1fb0409e9844f3b006895e
Uday Kushalapur <udaykushalapur55@gmail.com>
2026-02-03 20:25:55 +0530

    loops practise programs

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
 
-     
+
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+        int n=2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
     }
 }
