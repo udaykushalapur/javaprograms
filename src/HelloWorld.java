a80016f101087148091543a4374b7fac8c75247d
Uday Kushalapur <udaykushalapur55@gmail.com>
2026-02-03 19:24:16 +0530

    updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,16 +3,6 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-
-        int sum=0;
-
-        for(int i=1;i<=n;i++){
-            sum=sum+i;
-        }
-        System.out.println(sum);
-
+     
     }
 }
-
